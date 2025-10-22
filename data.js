1091527bc3e9f3ab6e749ac6a8a27ae20f9d5364
cedriccay <cedric.cayet@gmail.com>
2025-10-22 15:31:07 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,119 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0011557",
+      "name": "R0011557",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 840,
+      "initialViewParameters": {
+        "yaw": 0.19886022482468135,
+        "pitch": -0.016149109114962812,
+        "fov": 1.5494026726712853
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6234806495547964,
+          "pitch": 0.02127104034072147,
+          "rotation": 0,
+          "target": "1-r0011558"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0011558",
+      "name": "R0011558",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 840,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4327640650403328,
+          "pitch": 0.010566263078882088,
+          "rotation": 0,
+          "target": "0-r0011557"
+        },
+        {
+          "yaw": -2.3728453755990095,
+          "pitch": 0.07081939035712992,
+          "rotation": 0,
+          "target": "2-r0011559"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0011559",
+      "name": "R0011559",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 840,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9500080677100584,
+          "pitch": 0.3161822226322908,
+          "rotation": 0,
+          "target": "1-r0011558"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
